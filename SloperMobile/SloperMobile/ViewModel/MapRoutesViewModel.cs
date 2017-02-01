e8b3d6e0fc889cb608579bc072068b41965f345d
pratius.dubey <pratius.vin@gmail.com>
2017-02-01 21:42:21 +0530

    Working on Map Page

--- SloperMobile/SloperMobile/ViewModel/MapRoutesViewModel.cs
+++ SloperMobile/SloperMobile/ViewModel/MapRoutesViewModel.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using SloperMobile.Model;
 using Xamarin.Forms;
 using SloperMobile.Common.Command;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace SloperMobile.ViewModel
 {
@@ -34,6 +36,31 @@ namespace SloperMobile.ViewModel
         private void ExecuteOnSends(object obj)
         {
             OnPageNavigation?.Invoke();
+        }
+
+        private void LoadSectorImages()
+        {
+            try
+            {
+                var sector_images = App.DAUtil.GetSectorImages(1, 10);
+                foreach (var sector in sector_images)
+                {
+                    var topoimg = JsonConvert.DeserializeObject<TopoImageResponse>(sector.topo_json);
+                    string strimg64 = topoimg.image.data.Split(',')[1];
+                    if (!string.IsNullOrEmpty(strimg64))
+                    {
+                        MapListModel objSec = new MapListModel();
+                        objSec.SectorId = sector.sector_id;
+                        byte[] imageBytes = Convert.FromBase64String(strimg64);
+                        objSec.SectorImage = ImageSource.FromStream(() => new MemoryStream(imageBytes));
+                        
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string strerr = ex.Message;
+            }
         }
 
         public DelegateCommand SendCommand { get; set; }
