eb00d87b2e8384ef9be190dc767bfcd8848b4f9c
Ravindra Kumar <ravi.k@maanyait.com>
2017-02-02 14:00:00 +0530

    Ascent Process

---
+++ SloperMobile/SloperMobile.Droid/obj/Debug/android/src/md554b34bb83ebb7e18224badece0c2f62d/CalendarPickerView_OnPageChangeListener.java
@@ -0,0 +1,54 @@
+package md554b34bb83ebb7e18224badece0c2f62d;
+
+
+public class CalendarPickerView_OnPageChangeListener
+	extends android.support.v4.view.ViewPager.SimpleOnPageChangeListener
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onPageSelected:(I)V:GetOnPageSelected_IHandler\n" +
+			"";
+		mono.android.Runtime.register ("XLabs.Forms.Controls.MonoDroid.TimesSquare.CalendarPickerView+OnPageChangeListener, XLabs.Forms.Droid, Version=2.0.5610.35391, Culture=neutral, PublicKeyToken=null", CalendarPickerView_OnPageChangeListener.class, __md_methods);
+	}
+
+
+	public CalendarPickerView_OnPageChangeListener () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == CalendarPickerView_OnPageChangeListener.class)
+			mono.android.TypeManager.Activate ("XLabs.Forms.Controls.MonoDroid.TimesSquare.CalendarPickerView+OnPageChangeListener, XLabs.Forms.Droid, Version=2.0.5610.35391, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+	public CalendarPickerView_OnPageChangeListener (md554b34bb83ebb7e18224badece0c2f62d.CalendarPickerView p0) throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == CalendarPickerView_OnPageChangeListener.class)
+			mono.android.TypeManager.Activate ("XLabs.Forms.Controls.MonoDroid.TimesSquare.CalendarPickerView+OnPageChangeListener, XLabs.Forms.Droid, Version=2.0.5610.35391, Culture=neutral, PublicKeyToken=null", "XLabs.Forms.Controls.MonoDroid.TimesSquare.CalendarPickerView, XLabs.Forms.Droid, Version=2.0.5610.35391, Culture=neutral, PublicKeyToken=null", this, new java.lang.Object[] { p0 });
+	}
+
+
+	public void onPageSelected (int p0)
+	{
+		n_onPageSelected (p0);
+	}
+
+	private native void n_onPageSelected (int p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
