f434798e02e42b917d37d3ec8f814c61d59febc7
pratius.dubey <pratius.vin@gmail.com>
2017-02-04 02:33:44 +0530

    Working on Map Page

---
+++ SloperMobile/SloperMobile/Model/MapRouteImageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SloperMobile.Model
+{
+    public class MapRouteImageModel : BaseModel
+    {
+        public string ImagePath { get; set; }
+        public int Id { get; set; }
+    }
+}
--- SloperMobile/SloperMobile/ViewModel/MapRoutesViewModel.cs
+++ SloperMobile/SloperMobile/ViewModel/MapRoutesViewModel.cs
@@ -8,6 +8,8 @@ using Xamarin.Forms;
 using SloperMobile.Common.Command;
 using Newtonsoft.Json;
 using System.IO;
+using System.Collections.ObjectModel;
+using SloperMobile.Common.Constants;
 
 namespace SloperMobile.ViewModel
 {
@@ -37,13 +39,87 @@ namespace SloperMobile.ViewModel
             set { isPopupShow = value; OnPropertyChanged(); }
         }
 
+        private string _sectorName;
+
+        public string SectorName
+        {
+            get { return _sectorName; }
+            set { _sectorName = value; OnPropertyChanged(); }
+        }
+
+        private string cragName;
+
+        public string CragName
+        {
+            get { return cragName; }
+            set { cragName = value; OnPropertyChanged(); }
+        }
+
+        private string stateName;
+
+        public string StateName
+        {
+            get { return stateName; }
+            set { stateName = value; OnPropertyChanged(); }
+        }
+
+        private string routeName;
+
+        public string RouteName
+        {
+            get { return routeName; }
+            set { routeName = value; OnPropertyChanged(); }
+        }
+
+        private string techGrade;
+
+        public string TechGrade
+        {
+            get { return techGrade; }
+            set { techGrade = value; OnPropertyChanged(); }
+        }
+
+        private string routeInfo;
+
+        public string RouteInfo
+        {
+            get { return routeInfo; }
+            set { routeInfo = value; OnPropertyChanged(); }
+        }
+
+        private string rating;
+
+        public string Rating
+        {
+            get { return rating; }
+            set { rating = value; OnPropertyChanged(); }
+        }
+
+
+
+        private string routeTypeTop;
+
+        public string RouteTypeTop
+        {
+            get { return routeTypeTop; }
+            set { routeTypeTop = value; OnPropertyChanged(); }
+        }
+
+        private ObservableCollection<MapRouteImageModel> routeImageList;
+
+        public ObservableCollection<MapRouteImageModel> RouteImageList
+        {
+            get { return routeImageList; }
+            set { routeImageList = value; OnPropertyChanged(); }
+        }
+
 
         public MapRoutesViewModel(MapListModel CurrentSector)
         {
             SendCommand = new DelegateCommand(ExecuteOnSends);
             HidePopupCommand = new DelegateCommand(ExecuteOnHidePopup);
             ShowPopupCommand = new DelegateCommand(ExecuteOnShowPopup);
-            IsPopupHide = true;          
+            IsPopupHide = true;
             try
             {
                 PageHeaderText = CurrentSector.SectorName;
@@ -55,7 +131,7 @@ namespace SloperMobile.ViewModel
             }
         }
 
-   
+
 
         private void ExecuteOnSends(object obj)
         {
@@ -90,6 +166,16 @@ namespace SloperMobile.ViewModel
         private void LoadRouteDate()
         {
             var routeData = App.DAUtil.GetRouteDataByRouteID("22044");
+            if (routeData != null)
+            {
+                SectorName = routeData.route_name;
+                RouteName = routeData.route_name;
+                CragName = routeData.crag_id;
+                RouteInfo = routeData.route_info;
+                Rating = routeData.rating;
+                TechGrade = routeData.tech_grade;
+                LoadTheImageBaseOnRouteType(routeData.route_type);
+            }
         }
 
         private void ExecuteOnHidePopup(object obj)
@@ -102,6 +188,35 @@ namespace SloperMobile.ViewModel
         {
             IsPopupShow = true;
             IsPopupHide = false;
+        }
+
+        private void LoadTheImageBaseOnRouteType(string routeType)
+        {
+            RouteImageList = new ObservableCollection<MapRouteImageModel>();
+            if (string.IsNullOrWhiteSpace(routeType))
+                return;
+            if (routeType == AppConstant.RouteType_climbing_Angle)
+            {
+                RouteImageList.Add(new MapRouteImageModel { Id = 1, ImagePath = AppConstant.RouteType_Angle_Slab_1 });
+                RouteImageList.Add(new MapRouteImageModel { Id = 2, ImagePath = AppConstant.RouteType_Angle_Vertical_2 });
+                RouteImageList.Add(new MapRouteImageModel { Id = 3, ImagePath = AppConstant.RouteType_Angle_Overhanging_4 });
+                RouteImageList.Add(new MapRouteImageModel { Id = 4, ImagePath = AppConstant.RouteType_Angle_Roof_8 });
+            }
+            else if (routeType == AppConstant.RouteType_hold_Type)
+            {
+                RouteImageList.Add(new MapRouteImageModel { Id = 1, ImagePath = AppConstant.RouteType_Hold_Slopers_1 });
+                RouteImageList.Add(new MapRouteImageModel { Id = 2, ImagePath = AppConstant.RouteType_Hold_Crimp_2 });
+                RouteImageList.Add(new MapRouteImageModel { Id = 3, ImagePath = AppConstant.RouteType_Hold_Jungs_4 });
+                RouteImageList.Add(new MapRouteImageModel { Id = 4, ImagePath = AppConstant.RouteType_Hold_Pockets_8 });
+            }
+            else if (routeType == AppConstant.RouteType_Route_Style)
+            {
+                RouteImageList.Add(new MapRouteImageModel { Id = 1, ImagePath = AppConstant.RouteType_Route_Style_Technical_1 });
+                RouteImageList.Add(new MapRouteImageModel { Id = 2, ImagePath = AppConstant.RouteType_Route_Style_Sequential_2 });
+                RouteImageList.Add(new MapRouteImageModel { Id = 3, ImagePath = AppConstant.RouteType_Route_Style_Powerful_4 });
+                RouteImageList.Add(new MapRouteImageModel { Id = 4, ImagePath = AppConstant.RouteType_Route_Style_Sustained_8 });
+                RouteImageList.Add(new MapRouteImageModel { Id = 5, ImagePath = AppConstant.RouteType_Route_Style_One_Move_16 });
+            }
         }
 
         public DelegateCommand SendCommand { get; set; }
