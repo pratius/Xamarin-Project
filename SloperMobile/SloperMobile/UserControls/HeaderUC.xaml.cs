0b9990cfdd41194b1d49b9ef90887563cdf7420c
pratius.dubey <pratius.vin@gmail.com>
2017-02-13 22:50:26 +0530

    Workin On Bug

--- SloperMobile/SloperMobile/UserControls/HeaderUC.xaml.cs
+++ SloperMobile/SloperMobile/UserControls/HeaderUC.xaml.cs
@@ -22,10 +22,12 @@ namespace SloperMobile.UserControls
             Cache.MasterPage.IsPresented = true; 
         }
 
-        //private async void OnSearch(object sender, EventArgs e)
-        //{
-        //    await Navigation.PushAsync(new Views.SearchPage());
-        //}
+
+
+        private async void OnSearch(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Views.SearchPage());
+        }
 
 
 
