19aa9b47980d4c137249261c3284c6ea3bf80338
pratius.dubey <pratius.vin@gmail.com>
2017-02-19 18:51:59 +0530

    Working Map page

--- SloperMobile/SloperMobile/Views/TopoMapRoutesPage.xaml.cs
+++ SloperMobile/SloperMobile/Views/TopoMapRoutesPage.xaml.cs
@@ -15,14 +15,15 @@ namespace SloperMobile.Views
         {
             try
             {
-                InitializeComponent();
+                InitializeComponent();                
                 listData = _lstData;
                 NavigationPage.SetHasNavigationBar(this, true);
                 Title = CurrentSector.SectorName;
                 MapRouteVM = new ViewModel.MapRoutesViewModel(CurrentSector);
+                
                 BindingContext = MapRouteVM;
                 MapRouteVM.OnConditionNavigation = OnPageNavigation;
-
+                MapRouteVM.IsRunningTasks = true;
                 this.webView.RegisterCallback("dataCallback", t =>
                 Device.BeginInvokeOnMainThread(() =>
                 {
@@ -32,9 +33,10 @@ namespace SloperMobile.Views
             }
             catch (Exception ex)
             {
-
+                MapRouteVM.IsRunningTasks = false;
                 throw ex;
             }
+            MapRouteVM.IsRunningTasks = false;
         }
         private async void OnPageNavigation(object obj)
         {
@@ -49,12 +51,7 @@ namespace SloperMobile.Views
             this.webView.LoadFinished += OnLoadFinished;
             
         }
-
-        public void LoadWebViews()
-        {
-            this.webView.LoadFromContent("HTML/TopoResizeImage.html");
-            this.webView.LoadFinished += OnLoadFinished;
-        }
+             
         protected override void OnDisappearing()
         {
             base.OnDisappearing();
@@ -63,7 +60,7 @@ namespace SloperMobile.Views
         private void OnLoadFinished(object sender, EventArgs args)
         {
             bool IsRouteClicked = false;
-
+            MapRouteVM.IsRunningTasks = true;
             string staticAnnotationData = "[{\"AnnotationName\": \"Open Text (left)\",\"AnnotationType\": 0,\"ImageName\":\"\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"Open Text (right)\",\"AnnotationType\": 0,\"ImageName\":\"\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"Anchor (white)\",\"AnnotationType\": 0,\"ImageName\":\"\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"Anchor (black)\",\"AnnotationType\": 0,\"ImageName\":\"\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"Route Line\",\"AnnotationType\": 0,\"ImageName\":\"sample-line.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"Route Badge\",\"AnnotationType\": 1,\"ImageName\":\"sample-route-badge.png\",\"XCentreOffset\": -11,\"YCentreOffset\":-11},{\"AnnotationName\":\"Belay Point\",\"AnnotationType\": 2,\"ImageName\":\"sample-belay.png\",\"XCentreOffset\": -15,\"YCentreOffset\":-15},{\"AnnotationName\": \"Lower-off Left\",\"AnnotationType\": 3,\"ImageName\":\"sample-lower-off-left.png\",\"XCentreOffset\": -22,\"YCentreOffset\":-15},{\"AnnotationName\": \"Lower-off Right\",\"AnnotationType\": 4,\"ImageName\":\"sample-lower-off-right.png\",\"XCentreOffset\": -8,\"YCentreOffset\":-15},{\"AnnotationName\": \"Grade Label (left)\",\"AnnotationType\": 5,\"ImageName\":\"sample-grade-label-left.png\",\"XCentreOffset\": -5,\"YCentreOffset\":0},{\"AnnotationName\": \"Grade Label (right)\",\"AnnotationType\": 6,\"ImageName\":\"sample-grade-label-right.png\",\"XCentreOffset\": 5,\"YCentreOffset\":0},{\"AnnotationName\": \"Line Break\",\"AnnotationType\": 7,\"ImageName\":\"sample-route-line-break.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"Cross Point\",\"AnnotationType\": 8,\"ImageName\":\"x-mark-32.png\",\"XCentreOffset\": -50,\"YCentreOffset\":-50},{\"AnnotationName\": \"Text Add\",\"AnnotationType\": 9,\"ImageName\":\"tl-icon.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"TextAdd\",\"AnnotationType\": 10,\"ImageName\":\"tr-icon.png\",\"XCentreOffset\": -11,\"YCentreOffset\":-11},{\"AnnotationName\": \"TextRemove\",\"AnnotationType\": 11,\"ImageName\":\"tlcross-icon.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"TextRemove\",\"AnnotationType\": 12,\"ImageName\":\"trcross-icon.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"MoveLeft\",\"AnnotationType\": 13,\"ImageName\":\"move_left.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"MoveRight\",\"AnnotationType\": 14,\"ImageName\":\"move_right.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"icon-arrow\",\"AnnotationType\": 15,\"ImageName\":\"icon-arrow.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"cross dark black\",\"AnnotationType\": 16,\"ImageName\":\"cross_black.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"sample-lower-off-black-left\",\"AnnotationType\": 17,\"ImageName\":\"sample-lower-off-black-left.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"sample-lower-off-black-right\",\"AnnotationType\": 18,\"ImageName\":\"sample-lower-off-black-right.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0}]";
 
             // Added by sandeep
@@ -87,7 +84,19 @@ namespace SloperMobile.Views
             else
             {
                 webView.CallJsFunction("initRouteDrawing", staticAnnotationData, listData, "11840", (device.Display.Height - 250));
-            }                                 
+            }
+            MapRouteVM.IsRunningTasks = false;
+        }
+
+        private void SwipeableImage_SwipedDown(object sender, EventArgs e)
+        {
+            MapRouteVM.ShowPopupCommand.Execute(null);
+        }
+
+        private void SwipeableImage_SwipedUp(object sender, EventArgs e)
+        {
+            MapRouteVM.HidePopupCommand.Execute(null);
+
         }
     }
 }
