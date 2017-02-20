14af582b74115a575a09b737aec9f1bdd1d3f9e1
sandeep <sandeep@softechworld.com>
2017-02-20 07:14:45 -0700

    Carousel multi page and single page swipe issue fixed.

--- SloperMobile/SloperMobile/Views/TopoSectorPage.xaml.cs
+++ SloperMobile/SloperMobile/Views/TopoSectorPage.xaml.cs
@@ -15,6 +15,7 @@ namespace SloperMobile.Views
     {
         public MapListModel _CurrentSector { get; set; }
         private TopoSectorViewModel topoSectorViewModel;
+        public int _count = 0;       
         public TopoSectorPage(MapListModel CurrentSector)
         {
             InitializeComponent();
@@ -22,20 +23,25 @@ namespace SloperMobile.Views
             BindingContext = topoSectorViewModel;
             _CurrentSector = CurrentSector;
             //var topolistData = App.DAUtil.GetSectorLines(_CurrentSector?.SectorId);
-
-
         }
 
         protected async override void OnAppearing()
         {
             try
-            {
+            {                
                 this.Children.Clear();
                 topoSectorViewModel.IsRunningTasks = true;
+               
                 var topolistData = App.DAUtil.GetSectorLines(_CurrentSector?.SectorId);
                 var topoimgages = JsonConvert.DeserializeObject<List<TopoImageResponse>>(topolistData);
-                foreach (TopoImageResponse topores in topoimgages)
+                ContentPage newPage = new ContentPage();
+                if (topoimgages.Count == 1)
                 {
+                    _count = topoimgages.Count;                   
+                    this.Children.Add(newPage);
+                }
+                foreach (TopoImageResponse topores in topoimgages)
+                {                    
                     TopoMapRoutesPage topopageObj;
                     var topoimg = JsonConvert.SerializeObject(topores);
                     topopageObj = new TopoMapRoutesPage(_CurrentSector, "[" + topoimg + "]");
@@ -45,26 +51,30 @@ namespace SloperMobile.Views
                 await Task.Delay(2000);
                 this.SelectedItem = this.Children.LastOrDefault();
                 topoSectorViewModel.IsRunningTasks = false;
+                if (topoimgages.Count == 1)
+                {                   
+                    this.Children.Remove(newPage);
+                }
                 base.OnAppearing();
             }
             catch (Exception ex)
             {
 
                throw ex;
-            }
-        }
+            }            
+        }       
 
         protected override void OnCurrentPageChanged()
         {
-            base.OnCurrentPageChanged();
+            base.OnCurrentPageChanged();            
             var index = Children.IndexOf(CurrentPage);
             if (index > 0)
             {
-                this.SelectedItem = Children[0];
+                this.SelectedItem = Children[index];
             }
             if (index == 0)
-            {
-                this.SelectedItem = Children[0];
+            {               
+                this.SelectedItem = Children[Children.Count() - 1];
             }
         }
     }
