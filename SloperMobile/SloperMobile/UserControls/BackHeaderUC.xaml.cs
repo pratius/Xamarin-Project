07354c1964ab5db1f8560af58719b7913cefffff
Ravindra Kumar <ravikumarkhd1985@hotmail.com>
2017-03-01 11:54:52 +0530

    XAM-65 Fixed

---
+++ SloperMobile/SloperMobile/UserControls/BackHeaderUC.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace SloperMobile.UserControls
+{
+    public partial class BackHeaderUC : ContentView
+    {
+        public BackHeaderUC()
+        {
+            InitializeComponent();
+        }
+        private async void OnBackTapped(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync().ConfigureAwait(false);
+        }
+        private async void OnSearch(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Views.SearchPage());
+        }
+    }
+}
