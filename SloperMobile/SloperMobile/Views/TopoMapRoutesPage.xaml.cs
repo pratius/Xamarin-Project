4d040d49c57f6538905e4d572a7b4fa90b0542ae
sandeep <sandeep@softechworld.com>
2017-03-14 10:47:43 -0600

    XAM-107- Remove photo upload in ascent and fixed image height issue for iOS device.

--- SloperMobile/SloperMobile/Views/TopoMapRoutesPage.xaml.cs
+++ SloperMobile/SloperMobile/Views/TopoMapRoutesPage.xaml.cs
@@ -5,6 +5,8 @@ using XLabs.Platform.Device;
 using System.IO;
 using System.Net.Http;
 using SloperMobile.Common.Constants;
+using UIKit;
+using Foundation;
 
 namespace SloperMobile.Views
 {
@@ -14,6 +16,7 @@ namespace SloperMobile.Views
         private ViewModel.TopoMapRoutesViewModel TopoMapRouteVM;
         public string staticAnnotationData = "[{\"AnnotationName\": \"Open Text (left)\",\"AnnotationType\": 0,\"ImageName\":\"\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"Open Text (right)\",\"AnnotationType\": 0,\"ImageName\":\"\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"Anchor (white)\",\"AnnotationType\": 0,\"ImageName\":\"\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"Anchor (black)\",\"AnnotationType\": 0,\"ImageName\":\"\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"Route Line\",\"AnnotationType\": 0,\"ImageName\":\"sample-line.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"Route Badge\",\"AnnotationType\": 1,\"ImageName\":\"sample-route-badge.png\",\"XCentreOffset\": -11,\"YCentreOffset\":-11},{\"AnnotationName\":\"Belay Point\",\"AnnotationType\": 2,\"ImageName\":\"sample-belay.png\",\"XCentreOffset\": -15,\"YCentreOffset\":-15},{\"AnnotationName\": \"Lower-off Left\",\"AnnotationType\": 3,\"ImageName\":\"sample-lower-off-left.png\",\"XCentreOffset\": -22,\"YCentreOffset\":-15},{\"AnnotationName\": \"Lower-off Right\",\"AnnotationType\": 4,\"ImageName\":\"sample-lower-off-right.png\",\"XCentreOffset\": -8,\"YCentreOffset\":-15},{\"AnnotationName\": \"Grade Label (left)\",\"AnnotationType\": 5,\"ImageName\":\"sample-grade-label-left.png\",\"XCentreOffset\": -5,\"YCentreOffset\":0},{\"AnnotationName\": \"Grade Label (right)\",\"AnnotationType\": 6,\"ImageName\":\"sample-grade-label-right.png\",\"XCentreOffset\": 5,\"YCentreOffset\":0},{\"AnnotationName\": \"Line Break\",\"AnnotationType\": 7,\"ImageName\":\"sample-route-line-break.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"Cross Point\",\"AnnotationType\": 8,\"ImageName\":\"x-mark-32.png\",\"XCentreOffset\": -50,\"YCentreOffset\":-50},{\"AnnotationName\": \"Text Add\",\"AnnotationType\": 9,\"ImageName\":\"tl-icon.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"TextAdd\",\"AnnotationType\": 10,\"ImageName\":\"tr-icon.png\",\"XCentreOffset\": -11,\"YCentreOffset\":-11},{\"AnnotationName\": \"TextRemove\",\"AnnotationType\": 11,\"ImageName\":\"tlcross-icon.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"TextRemove\",\"AnnotationType\": 12,\"ImageName\":\"trcross-icon.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"MoveLeft\",\"AnnotationType\": 13,\"ImageName\":\"move_left.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"MoveRight\",\"AnnotationType\": 14,\"ImageName\":\"move_right.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"icon-arrow\",\"AnnotationType\": 15,\"ImageName\":\"icon-arrow.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"cross dark black\",\"AnnotationType\": 16,\"ImageName\":\"cross_black.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"sample-lower-off-black-left\",\"AnnotationType\": 17,\"ImageName\":\"sample-lower-off-black-left.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"sample-lower-off-black-right\",\"AnnotationType\": 18,\"ImageName\":\"sample-lower-off-black-right.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0}]";
         string listData = string.Empty;
+        Double height, newHeight;
         int _routeId = 0;
         public TopoMapRoutesPage(MapListModel CurrentSector, string _lstData, int routeId)
         {
@@ -45,7 +48,9 @@ namespace SloperMobile.Views
                     TopoMapRouteVM.LoadRouteData(t, listData);
                     TopoMapRouteVM.IsPopupHide = true;
                     var device = XLabs.Ioc.Resolver.Resolve<IDevice>();
-                    webView.CallJsFunction("initReDrawing", staticAnnotationData, listData, Cache.CurrentScreenHeight, Convert.ToInt32(t), true);
+                    height = device.Display.Height;
+                    newHeight = GetHeight(height);
+                    webView.CallJsFunction("initReDrawing", staticAnnotationData, listData, newHeight, Convert.ToInt32(t), true);
                 }));
             }
             catch (Exception ex)
@@ -60,6 +65,46 @@ namespace SloperMobile.Views
             await Navigation.PushAsync(new AscentProcessPage(Convert.ToString(obj), _CurrentSector));
         }
 
+        public double GetHeight(Double _height)
+        {
+            if (Device.OS != TargetPlatform.Android)
+            {
+                switch (Convert.ToInt32(_height))
+                {
+                    case 1334:
+                        _height = _height + 150; //iphone 6 and iphone 7 and iphone 6s
+                        break;
+                    case 2208:
+                        _height = _height - 700; //iPhone6Plus and iPhone7Plus and iphone 6s plus
+                        break;
+                    case 1136:
+                        _height = _height + 290; //iPhone5 and iPhone5s and iPhone SE
+                        break;
+                    case 2048:
+                        _height = _height - 900; //iPas Air 2 and iPad Air 
+                        break;
+                }
+            }
+            else
+            {
+                switch (Convert.ToInt32(height))
+                {
+                    case 1184:
+                        _height = _height - 725; //slopetTest
+                        break;
+                        //case 2208:
+                        //    _height = _height - 700; //iPhone6Plus and iPhone7Plus and iphone 6s plus
+                        //    break;
+                        //case 1136:
+                        //    _height = _height + 290; //iPhone5 and iPhone5s and iPhone SE
+                        //    break;
+                        //case 2048:
+                        //    _height = _height - 900; //iPas Air 2 and iPad Air 
+                        //    break;
+                }
+            }
+            return _height;
+        }
         protected override void OnAppearing()
         {
             try
@@ -85,22 +130,23 @@ namespace SloperMobile.Views
             bool IsRouteClicked = false;
             TopoMapRouteVM.IsRunningTasks = true;
             staticAnnotationData = "[{\"AnnotationName\": \"Open Text (left)\",\"AnnotationType\": 0,\"ImageName\":\"\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"Open Text (right)\",\"AnnotationType\": 0,\"ImageName\":\"\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"Anchor (white)\",\"AnnotationType\": 0,\"ImageName\":\"\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"Anchor (black)\",\"AnnotationType\": 0,\"ImageName\":\"\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"Route Line\",\"AnnotationType\": 0,\"ImageName\":\"sample-line.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"Route Badge\",\"AnnotationType\": 1,\"ImageName\":\"sample-route-badge.png\",\"XCentreOffset\": -11,\"YCentreOffset\":-11},{\"AnnotationName\":\"Belay Point\",\"AnnotationType\": 2,\"ImageName\":\"sample-belay.png\",\"XCentreOffset\": -15,\"YCentreOffset\":-15},{\"AnnotationName\": \"Lower-off Left\",\"AnnotationType\": 3,\"ImageName\":\"sample-lower-off-left.png\",\"XCentreOffset\": -22,\"YCentreOffset\":-15},{\"AnnotationName\": \"Lower-off Right\",\"AnnotationType\": 4,\"ImageName\":\"sample-lower-off-right.png\",\"XCentreOffset\": -8,\"YCentreOffset\":-15},{\"AnnotationName\": \"Grade Label (left)\",\"AnnotationType\": 5,\"ImageName\":\"sample-grade-label-left.png\",\"XCentreOffset\": -5,\"YCentreOffset\":0},{\"AnnotationName\": \"Grade Label (right)\",\"AnnotationType\": 6,\"ImageName\":\"sample-grade-label-right.png\",\"XCentreOffset\": 5,\"YCentreOffset\":0},{\"AnnotationName\": \"Line Break\",\"AnnotationType\": 7,\"ImageName\":\"sample-route-line-break.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"Cross Point\",\"AnnotationType\": 8,\"ImageName\":\"x-mark-32.png\",\"XCentreOffset\": -50,\"YCentreOffset\":-50},{\"AnnotationName\": \"Text Add\",\"AnnotationType\": 9,\"ImageName\":\"tl-icon.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"TextAdd\",\"AnnotationType\": 10,\"ImageName\":\"tr-icon.png\",\"XCentreOffset\": -11,\"YCentreOffset\":-11},{\"AnnotationName\": \"TextRemove\",\"AnnotationType\": 11,\"ImageName\":\"tlcross-icon.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"TextRemove\",\"AnnotationType\": 12,\"ImageName\":\"trcross-icon.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"MoveLeft\",\"AnnotationType\": 13,\"ImageName\":\"move_left.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"MoveRight\",\"AnnotationType\": 14,\"ImageName\":\"move_right.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"icon-arrow\",\"AnnotationType\": 15,\"ImageName\":\"icon-arrow.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"cross dark black\",\"AnnotationType\": 16,\"ImageName\":\"cross_black.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"sample-lower-off-black-left\",\"AnnotationType\": 17,\"ImageName\":\"sample-lower-off-black-left.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0},{\"AnnotationName\": \"sample-lower-off-black-right\",\"AnnotationType\": 18,\"ImageName\":\"sample-lower-off-black-right.png\",\"XCentreOffset\": 0,\"YCentreOffset\":0}]";
-
-            // Added by sandeep
-            // To load slider we need to pass all topo image data, I am using static which we can convert in dynamic 
-            // if image topo data will be single slider will not show and direct route page load with line drawing
-
             var device = XLabs.Ioc.Resolver.Resolve<IDevice>();
+
+            height = device.Display.Height;
+
+            newHeight = GetHeight(height);
             if (!IsRouteClicked)
             {
+
                 //webView.CallJsFunction("initDrawing", staticAnnotationData, listData, (device.Display.Height));
                 if (Device.OS == TargetPlatform.Android)
                 {
-                    webView.CallJsFunction("initDrawing", staticAnnotationData, listData, Cache.CurrentScreenHeight);
+
+                    webView.CallJsFunction("initDrawing", staticAnnotationData, listData, newHeight);
                 }
                 else
                 {
-                    webView.CallJsFunction("initDrawing", staticAnnotationData, listData, Cache.CurrentScreenHeight);
+                    webView.CallJsFunction("initDrawing", staticAnnotationData, listData, newHeight);
                 }
                 if (_routeId > 0)
                 {
@@ -110,11 +156,11 @@ namespace SloperMobile.Views
 
                     if (Device.OS == TargetPlatform.Android)
                     {
-                        webView.CallJsFunction("initReDrawing", staticAnnotationData, listData, (Cache.CurrentScreenHeight), _routeId, true);                        
+                        webView.CallJsFunction("initReDrawing", staticAnnotationData, listData, (newHeight), _routeId, true);
                     }
                     else
                     {
-                        webView.CallJsFunction("initReDrawing", staticAnnotationData, listData, (Cache.CurrentScreenHeight), _routeId, true);
+                        webView.CallJsFunction("initReDrawing", staticAnnotationData, listData, (newHeight), _routeId, true);
                     }
                 }
             }
@@ -122,11 +168,11 @@ namespace SloperMobile.Views
             {
                 if (Device.OS == TargetPlatform.Android)
                 {
-                    webView.CallJsFunction("initRouteDrawing", staticAnnotationData, listData, _routeId, (Cache.CurrentScreenHeight));                   
+                    webView.CallJsFunction("initRouteDrawing", staticAnnotationData, listData, _routeId, (newHeight));
                 }
                 else
                 {
-                    webView.CallJsFunction("initRouteDrawing", staticAnnotationData, listData, _routeId, (Cache.CurrentScreenHeight));
+                    webView.CallJsFunction("initRouteDrawing", staticAnnotationData, listData, _routeId, (newHeight));
                 }
             }
             TopoMapRouteVM.IsRunningTasks = false;
@@ -136,7 +182,7 @@ namespace SloperMobile.Views
         private void OnSwipeUpPopup(object sender, EventArgs e)
         {
             if (listData != string.Empty)
-            { 
+            {
                 TopoMapRouteVM.HidePopupCommand.Execute(null);
             }
         }
@@ -150,7 +196,7 @@ namespace SloperMobile.Views
         {
             TopoMapRouteVM.IsPopupHide = false;
             if (_routeId <= 0)
-                webView.CallJsFunction("initDrawing", staticAnnotationData, listData, Cache.CurrentScreenHeight);
+                webView.CallJsFunction("initDrawing", staticAnnotationData, listData, newHeight);
         }
     }
 }
