9c5096e5cf78d4aacaa158e9f734b540b86489cc
Steve Chaba <steve@slicksystems.ca>
2017-03-18 17:08:38 -0600

    Updated Sends List to only include current APP asents

--- SloperMobile/SloperMobile/Common/Constants/AppConstant.cs
+++ SloperMobile/SloperMobile/Common/Constants/AppConstant.cs
@@ -11,7 +11,7 @@ namespace SloperMobile.Common.Constants
     {
         public const string APP_TITLE = "Grand Valley Climbing";
         public const string APP_ID = "26";
-        public const string APP_DBNAME = "sloperdb.db3";
+        public const string APP_DBNAME = "slopergvcdb.db3";
         public const string APP_COMPANY = "Grand Valley Climbing";
 
         public const string EMPTY_STRING = "";
@@ -25,8 +25,8 @@ namespace SloperMobile.Common.Constants
         public const string SPF_ACCESSTOKEN = "accessToken";
         public const string SPF_RENEWALTOKEN = "renewalToken";
 
-        public const string Guest_UserId = "sloper.anonymous";
-        public const string Guest_UserPassword = "s.s2016!";
+        public const string Guest_UserId = "gvc.anonymous@sloperclimbing.com";
+        public const string Guest_UserPassword = "s.s2017!";
 
         //MapRoutePage
         public const string RouteType_climbing_Angle = "climbing angle";
