283a1e2249b9e996dd5edf89c8e080c3c0351fd4
Ravindra Kumar <ravikumarkhd1985@hotmail.com>
2017-05-06 01:50:22 +0530

    AppSetting Updated

--- SloperMobile/SloperMobile/Common/Constants/AppSetting.cs
+++ SloperMobile/SloperMobile/Common/Constants/AppSetting.cs
@@ -9,22 +9,22 @@ namespace SloperMobile.Common.Constants
     public class AppSetting
     {
         //API Base URL
-        public const string Base_Url = "http://sloper.slicksystems.ca:8080/DesktopModules/";
-        public const string APP_ID = "1";
+        public const string Base_Url = "http://www.sloperclimbing.com/DesktopModules/";
+        public const string APP_ID = "32";
         public const string APP_TYPE = "indoor";
 
         //HockeyApp
-        public const string HockeyAppId_Droid = "c528525a7d654946a5692d82f19a8228";
-        public const string HockeyAppId_iOS = "12da4aa9ca2044228df1cca19fd7675d";
+        public const string HockeyAppId_Droid = "5938f69b0fb243bcaf30f9df53975c7d";
+        public const string HockeyAppId_iOS = "6cbf84c91c994e4fa5ff9277dbde61d3";
 
         //Company
-        public const string APP_TITLE = "BULL IN A CHINA SHOP";
-        public const string APP_COMPANY = "Bull in a China Shop";
+        public const string APP_TITLE = "URBAN CLIMB";
+        public const string APP_COMPANY = "Urban Climb";
         public const string APP_DBNAME = "sloper" + APP_ID + ".db3";
-        public const string APP_LABEL_DROID = "Bull App";
+        public const string APP_LABEL_DROID = "Urban Climb";
 
         //Guest DNN Login
-        public const string Guest_UserId = "bull.guest@sloperclimbing.com";
-        public const string Guest_UserPassword = "G'7/[LdYbcC4K^Jr";
+        public const string Guest_UserId = "urbanclimb.guest@sloperclimbing.com";
+        public const string Guest_UserPassword = "$KG!je,L4uJ8U8}!";
     }
 }
