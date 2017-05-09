d2658b76dd863c2a30e75651b3c9a9eede6f5ff5
Steve Chaba <steve@slicksystems.ca>
2017-05-09 16:49:24 -0600

    Added navSectors Image

---
+++ SloperMobile/SloperMobile/Views/SlickTest.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SloperMobile
+{
+    public partial class SlickTest : ContentPage
+    {
+        public SlickTest()
+        {
+            InitializeComponent();
+        }
+    }
+}
