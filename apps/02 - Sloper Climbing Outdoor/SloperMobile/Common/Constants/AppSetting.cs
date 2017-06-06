7979fc854a53e81dda541a8dd2cc5c8c549e1603
Steve Chaba <steve@slicksystems.ca>
2017-06-06 15:35:58 -0600

    Updated Icons and removed old default images

--- apps/02 - Sloper Climbing Outdoor/SloperMobile/Common/Constants/AppSetting.cs
+++ apps/02 - Sloper Climbing Outdoor/SloperMobile/Common/Constants/AppSetting.cs
@@ -6,24 +6,39 @@ using System.Threading.Tasks;
 
 namespace SloperMobile.Common.Constants
 {
-    public class AppSetting
-    {
-        //API Base URL
-        public const string Base_Url = "http://www.sloperclimbing.com/DesktopModules/";
-        public const string APP_ID = "32";
+	public class AppSetting
+	{
+		//API Base URL
+		public const string API_VERSION = "v170428";
+		public const string Base_Url = "http://www.sloperclimbing.com/DesktopModules/";
+		public const string APP_ID = "2";
+		public const string APP_TYPE = "outdoor";
 
-        //HockeyApp
-        public const string HockeyAppId_Droid = "5938f69b0fb243bcaf30f9df53975c7d";
-        public const string HockeyAppId_iOS = "6cbf84c91c994e4fa5ff9277dbde61d3";
+		//HockeyApp
+		public const string HockeyAppId_Droid = "7f4a57b534554e3abf9d0a7d45e7d9e0";
+		public const string HockeyAppId_iOS = "51f531c5ab89457aa5b731a9edeca878";
 
-        //Company
-        public const string APP_TITLE = "URBAN CLIMB";
-        public const string APP_COMPANY = "Urban Climb";
-        public const string APP_DBNAME = "sloper" + APP_ID + ".db3";
-        public const string APP_LABEL_DROID = "Urban Climb";
+		//Google Map API Key Ravi
+		//public const string GoogleApiKey_Droid = "AIzaSyDlezhB_TB6imlElBbrajrIEnvmdtAhfF8";
+		public const string GoogleApiKey_Droid = "AIzaSyASierV3-kHa3WE6R6KGJg556J0CAFcoT8";
+		public const string GoogleApiKey_iOS = "AIzaSyDArJKC42cFUb_LThCM-UmY5BhO06BVOD4";
 
-        //Guest DNN Login
-        public const string Guest_UserId = "urbanclimb.guest@sloperclimbing.com";
-        public const string Guest_UserPassword = "$KG!je,L4uJ8U8}!";
-    }
+		//Google Map API Key Steve
+		//public const string GoogleApiKey_Droid = "AIzaSyDJNaoMh4e5cQyQrqeFHlP3r_Avco1AOQ4";
+		//public const string GoogleApiKey_iOS = "AIzaSyBom0uG454iMScJX3v9yQdXM47524qjdIU";
+
+		//Company
+		public const string APP_TITLE = "SLOPER CLIMBING";
+		public const string APP_COMPANY = "Sloper Climbing";
+		public const string APP_DBNAME = "sloper" + APP_ID + ".db3";
+		public const string APP_LABEL_DROID = "Sloper Climbing";
+
+		//Guest DNN Login
+		//public const string Guest_UserId = "bull.guest@sloperclimbing.com";
+		//public const string Guest_UserPassword = "G'7/[LdYbcC4K^Jr";
+
+		//Guest DNN Login
+		public const string Guest_UserId = "sloper.anonymous";
+		public const string Guest_UserPassword = "s.s2016!";
+	}
 }
