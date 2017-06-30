6c724241f0c27f8bf2d1258b7581f38f94177093
Steve Chaba <steve@slicksystems.ca>
2017-06-29 23:55:03 -0600

    Updated AppSettings to default to the dev data.
    *Please don't commit this file if you change it.

--- SloperMobile/SloperMobile/Common/Constants/AppSetting.cs
+++ SloperMobile/SloperMobile/Common/Constants/AppSetting.cs
@@ -1,67 +1,65 @@
-﻿#region Use For Live Data
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace SloperMobile.Common.Constants
 {
     public class AppSetting
     {
         //API Base URL
-        public const string Base_Url = "http://www.sloperclimbing.com/DesktopModules/";
+        public const string Base_Url = "http://sloper.slicksystems.ca:8080/DesktopModules/";
         public const string API_VERSION = "v170428";
-        public const string APP_ID = "20";
-        public const string APP_TYPE = "indoor";
-        public const string APP_DBNAME = "sloper" + APP_ID + ".db3";        //HockeyApp
-        public const string HockeyAppId_Droid = "3d57b8a3ebcd41d9b28c07fe8577daef";
-        public const string HockeyAppId_iOS = "747f9835300a4aed9d10818c885a1e04";        //Google Map API Key Steve
+        public const string APP_ID = "1";
+        public const string APP_TYPE = "outdoor";
+        public const string APP_DBNAME = "sloper" + APP_ID + ".db3";
+
+        //HockeyApp
+        public const string HockeyAppId_Droid = "c528525a7d654946a5692d82f19a8228";
+        public const string HockeyAppId_iOS = "12da4aa9ca2044228df1cca19fd7675d";
+
+        //Google Map API Key Steve
         public const string GoogleApiKey_Droid = "AIzaSyC45C1WQGYjmePYECElEWl4k-Zr3YDQQdU";
-        public const string GoogleApiKey_iOS = "AIzaSyDFYT8I-UVDNHYru4Cvold-bZJE3M9UWas";        //Company
-        public const string APP_TITLE = "ELEVATION PLACE";
-        public const string APP_COMPANY = "Elevation Place";
-        public const string APP_LABEL_DROID = "Elevation Place";        //Guest DNN Login
-        public const string Guest_UserId = "elevationplace.guest@sloperclimbing.com";
-        public const string Guest_UserPassword = "6';_>UQqgE3pXLw-";
+        public const string GoogleApiKey_iOS = "AIzaSyDFYT8I-UVDNHYru4Cvold-bZJE3M9UWas";
+
+        //Company
+        public const string APP_TITLE = "BULL IN A CHINA SHOP";
+        public const string APP_COMPANY = "Bull in a China Shop";
+        public const string APP_LABEL_DROID = "Bull App";
+
+        //Guest DNN Login
+        public const string Guest_UserId = "bull.guest@sloperclimbing.com";
+        public const string Guest_UserPassword = "G'7/[LdYbcC4K^Jr";
     }
 }
-#endregion
 
-
+//#region Use For Live Data
 
 //using System;
 //using System.Collections.Generic;
 //using System.Linq;
 //using System.Text;
 //using System.Threading.Tasks;
-
 //namespace SloperMobile.Common.Constants
 //{
 //    public class AppSetting
 //    {
 //        //API Base URL
-//        public const string Base_Url = "http://sloper.slicksystems.ca:8080/DesktopModules/";
+//        public const string Base_Url = "http://www.sloperclimbing.com/DesktopModules/";
 //        public const string API_VERSION = "v170428";
-//        public const string APP_ID = "1";
-//        public const string APP_TYPE = "outdoor";
-//        public const string APP_DBNAME = "sloper" + APP_ID + ".db3";
-
-//        //HockeyApp
-//        public const string HockeyAppId_Droid = "c528525a7d654946a5692d82f19a8228";
-//        public const string HockeyAppId_iOS = "12da4aa9ca2044228df1cca19fd7675d";
-
-//        //Google Map API Key Steve
+//        public const string APP_ID = "20";
+//        public const string APP_TYPE = "indoor";
+//        public const string APP_DBNAME = "sloper" + APP_ID + ".db3";        //HockeyApp
+//        public const string HockeyAppId_Droid = "3d57b8a3ebcd41d9b28c07fe8577daef";
+//        public const string HockeyAppId_iOS = "747f9835300a4aed9d10818c885a1e04";        //Google Map API Key Steve
 //        public const string GoogleApiKey_Droid = "AIzaSyC45C1WQGYjmePYECElEWl4k-Zr3YDQQdU";
-//        public const string GoogleApiKey_iOS = "AIzaSyDFYT8I-UVDNHYru4Cvold-bZJE3M9UWas";
-
-//        //Company
-//        public const string APP_TITLE = "BULL IN A CHINA SHOP";
-//        public const string APP_COMPANY = "Bull in a China Shop";
-//        public const string APP_LABEL_DROID = "Bull App";
-
-//        //Guest DNN Login
-//        public const string Guest_UserId = "bull.guest@sloperclimbing.com";
-//        public const string Guest_UserPassword = "G'7/[LdYbcC4K^Jr";
+//        public const string GoogleApiKey_iOS = "AIzaSyDFYT8I-UVDNHYru4Cvold-bZJE3M9UWas";        //Company
+//        public const string APP_TITLE = "ELEVATION PLACE";
+//        public const string APP_COMPANY = "Elevation Place";
+//        public const string APP_LABEL_DROID = "Elevation Place";        //Guest DNN Login
+//        public const string Guest_UserId = "elevationplace.guest@sloperclimbing.com";
+//        public const string Guest_UserPassword = "6';_>UQqgE3pXLw-";
 //    }
 //}
+//#endregion
