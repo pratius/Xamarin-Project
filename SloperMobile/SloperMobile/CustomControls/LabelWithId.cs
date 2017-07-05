23dc90e4c0623e80e1662092e4c1ce06cc5dcb10
Roman <Roman@SLICKSYSTEMS>
2017-07-05 07:30:16 -0600

    local changes

--- SloperMobile/SloperMobile/CustomControls/LabelWithId.cs
+++ SloperMobile/SloperMobile/CustomControls/LabelWithId.cs
@@ -2,13 +2,65 @@
 
 namespace SloperMobile.CustomControls
 {
-    public class LabelWithId : Label
+    public class GridWithId : Grid
     {
-        public LabelWithId(long id)
+		string color;
+		string routeCount;
+		int labelTextFontSize;
+
+        public GridWithId(long id, string color, string routeCount, int labelTextFontSize)
         {
             PointId = id;
+			this.color = color;
+			this.routeCount = routeCount;
+			this.labelTextFontSize = labelTextFontSize;
         }
 
         public long PointId { get; set; }
-    }
+
+		protected override void OnParentSet()
+		{
+			base.OnParentSet();
+			var innerBoxView = new BoxView
+			{
+				HeightRequest = 20,
+				WidthRequest = 20,
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				BackgroundColor = Color.White,
+				Color = Color.White,
+				Rotation = 40
+			};
+
+			var boxView = new BoxView
+			{
+				HeightRequest = 18,
+				WidthRequest = 18,
+				Color = Color.FromHex(color),
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				Rotation = 40,
+				AutomationId = PointId.ToString(),
+			};
+
+			var labelWithId = new Label();
+			labelWithId.HorizontalTextAlignment = TextAlignment.Center;
+			labelWithId.VerticalTextAlignment = TextAlignment.Center;
+			labelWithId.Text = routeCount;
+			labelWithId.TextColor = Color.White;
+			labelWithId.FontSize = labelTextFontSize;
+
+			Children.Add(innerBoxView);
+			Grid.SetRow(innerBoxView, 0);
+			Grid.SetColumn(innerBoxView, 0);
+
+			Children.Add(boxView);
+			Grid.SetRow(boxView, 0);
+			Grid.SetColumn(boxView, 0);
+
+			Children.Add(labelWithId);
+			Grid.SetRow(labelWithId, 0);
+			Grid.SetColumn(labelWithId, 0);
+		}
+	}
 }
